f74e0f52e564b23882891cb32f11a452d77627a1
nondumiso166 <220430853@mycput.ac.za>
2022-04-13 22:42:22 +0200

    adding files

---
+++ app/src/main/java/za/ac/cput/jetcompose/MainActivity.kt
@@ -0,0 +1,92 @@
+/*
+Nondumiso Gaga (220430853)
+
+ */
+package za.ac.cput.jetcompose
+
+
+import android.graphics.fonts.FontStyle
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily.Companion.Cursive
+import androidx.compose.ui.text.font.FontStyle.Companion.Italic
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import za.ac.cput.jetcompose.ui.theme.JetcomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+     setContent{ ScreenText()}
+    }}
+
+
+@Composable
+@Preview
+fun ScreenText(){
+    val visible: MutableState<Boolean> = remember {mutableStateOf(false)}
+   ShowDialog(visible = visible)
+    Column(
+        modifier= Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top) {
+        Text(
+            "Welcome to my jetpack compose journey",
+            color = Color.Magenta,fontSize = 30.sp ,
+            fontWeight = FontWeight.Bold
+        )}
+    Column(
+        modifier= Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement=Arrangement.Center) {
+        Button(onClick = { visible.value= true })
+        {
+            Icon(
+                imageVector = Icons.Filled.Info,
+                contentDescription = null,
+                Modifier.padding(end = 9.dp)
+            )
+            Text(text = " Info")
+        }
+    }
+    }
+
+@Composable
+fun ShowDialog(visible:MutableState<Boolean>){
+    if(visible.value){
+        AlertDialog(
+            onDismissRequest = { visible.value= false },dismissButton = {
+            TextButton(onClick = { visible.value=false }) {
+                Text(text= "Cancel")
+
+        }},confirmButton={TextButton(onClick = { visible.value=false }) {
+                Text(text= "OK")
+
+            }},
+            title= { Text(text = "My jetpack Journey")},
+            text={Text (text="This is my first jetpack compose application, I am hoping that with more practice i will buil a much better application and will improve :)")}
+        )
+        }
+    }
+
+
